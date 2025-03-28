ac96cc543bcb4c7c45ee8b72f7301cd274b36cb3
Farhat <f190886@mail.ru>
2025-03-28 11:30:02 +0400

    dobavlenie selenium manager

--- conftest.py
+++ conftest.py
@@ -1,45 +1,27 @@
-from selenium.webdriver import Firefox
+from selenium import webdriver
+import pytest
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
-import pytest
-
 
 
 @pytest.fixture(scope='module')
 def driver():
-    global driver
     options = Options()
-
-    # Основные настройки
     options.add_argument("--width=1920")
     options.add_argument("--height=1080")
     options.add_argument("--headless")
 
-    # Критически важные параметры
-    options.set_preference("browser.tabs.remote.autostart", False)
-    options.set_preference("browser.tabs.remote.autostart.1", False)
-    options.set_preference("browser.tabs.remote.autostart.2", False)
-    options.set_preference("browser.tabs.remote.force-enable", False)
-
-    # Настройки для CI
-    options.set_preference("network.http.response.timeout", 300)
-    options.set_preference("dom.max_script_run_time", 300)
-
     service = Service(
-        executable_path=GeckoDriverManager().install(),
-        service_args=['--log', 'trace'],
-        timeout=300
+        GeckoDriverManager().install(),
+        service_args=['--log', 'debug'],
+        timeout=300  # Увеличенный таймаут
     )
 
-    try:
-        driver = Firefox(service=service, options=options)
-        driver.set_page_load_timeout(60)
-        driver.implicitly_wait(30)
-        yield driver
-    finally:
-        if 'driver' in locals():
-            driver.quit()
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.set_page_load_timeout(60)
+    yield driver
+    driver.quit()
 
 
 
